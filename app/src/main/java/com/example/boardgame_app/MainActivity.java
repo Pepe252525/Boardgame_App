c243c3d007d055d12fda77ab60710d6f909f095c
Pepe252525 <peter.peterlin@iu-study.org>
2026-04-09 11:57:06 +0200

    Hauptactivity Button Logik hinzugefügt und Strings.xml angepasst.

--- app/src/main/java/com/example/boardgame_app/MainActivity.java
+++ app/src/main/java/com/example/boardgame_app/MainActivity.java
@@ -1,17 +1,46 @@
 package com.example.boardgame_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
+    Button btnVote = findViewById(R.id.btnSpielevote);
+    Button btnTermine = findViewById(R.id.btnTermine);
+    Button btnVorschlag = findViewById(R.id.btnSpielvorschlaege);
+    Button btnChat = findViewById(R.id.btnChat);
+    Button btnSpieler = findViewById(R.id.btnSpielerliste);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        btnVote.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, GameVoteActivity.class));
+        });
+
+        btnTermine.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, EventActivity.class));
+        });
+
+        btnVorschlag.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, AddGameActivity.class));
+        });
+
+        btnChat.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, ChatActivity.class));
+        });
+
+        btnSpieler.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, PlayerListActivity.class));
+        });
+
     }
 }
